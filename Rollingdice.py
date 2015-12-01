ad74d4e80a6cb7142afd1623ed8ef2b960d78d6c
Paul Ijagbemi <paulijagbemi@seoulchristian.org>
2015-12-01 19:11:20 +0900

    ProgressSummary

---
+++ Rollingdice.py
@@ -0,0 +1,42 @@
+while True:
+    import random
+    print ("This is a dice rolling program")
+    print ("Press the enter button to roll")
+    cmd = raw_input ("Enter to run, q to quit")
+    number=random.randint(1,6)
+    if number==1:
+        print"[------------]"
+        print"[            ]"
+        print"[      O     ]"
+        print"[            ]"
+        print"[------------]"
+    if number==2:
+        print"[------------]"
+        print"[            ]"
+        print"[    O    O  ]"
+        print"[            ]"
+        print"[------------]"
+    if number==3:
+        print"[------------]"
+        print"[   O        ]"
+        print"[    O       ]"
+        print"[       O    ]"
+        print"[------------]"
+    if number==4:
+        print"[------------]"
+        print"[    O    O  ]"
+        print"[            ]"
+        print"[    O    O  ]"
+        print"[------------]"
+    if number==5:
+        print"[------------]"
+        print"[   O    O   ]"
+        print"[     O      ]"
+        print"[   O    O   ]"
+        print"[------------]"
+    if number==6:
+        print"[------------]"
+        print"[   O     O  ]"
+        print"[   O     O  ]"
+        print"[   O     O  ]"
+        print"[------------]"
